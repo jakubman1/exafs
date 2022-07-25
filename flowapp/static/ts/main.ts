8102024fddfd585eb897eaea2c0e3334ef17010a
Jakub Man <jakubman1@gmail.com>
2022-07-25 12:25:17 +0200

    add locally compiled Bootstrap

---
+++ flowapp/static/ts/main.ts
@@ -0,0 +1,8 @@
+import {Tooltip} from "bootstrap";
+
+window.onload = function () {
+    let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new Tooltip(tooltipTriggerEl)
+    })
+}
