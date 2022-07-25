874169b9636aa99c4e0c856c4a50873d382b770f
Jakub Man <jakubman1@gmail.com>
2022-07-25 12:52:16 +0200

    add function to attach a HTML element as a child to another element

--- flowapp/static/ts/renderer.ts
+++ flowapp/static/ts/renderer.ts
@@ -10,3 +10,21 @@ export function stringToHtml(str: string): HTMLCollection {
     // Parser adds a <body> tag to the document, we just want the children
     return doc.body.children;
 }
+
+/***
+ * Attach HTML to another element.
+ *
+ * @param {HTMLElement | HTMLCollection} html - HTML object to attach
+ * @param {string}                       ref  - HTML element to attach the new HTML to
+ */
+export function attachHtmlToRef(html: HTMLElement | HTMLCollection, ref: HTMLElement) {
+    if (html instanceof HTMLElement) {
+        ref.appendChild(html);
+    } else if (html instanceof HTMLCollection) {
+        let arr = Array.from(html);
+        for (let i = 0; i < arr.length; i++) {
+            const item = arr[i];
+            ref.appendChild(item);
+        }
+    }
+}
