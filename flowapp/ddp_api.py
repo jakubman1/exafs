4763d50117ddbb97d385d4dfdab9f9517742e6ce
Jakub Man <jakubman1@gmail.com>
2022-07-26 15:55:03 +0200

    add function to get rule from DDoS Protector

--- flowapp/ddp_api.py
+++ flowapp/ddp_api.py
@@ -37,3 +37,20 @@ def remove_rule_from_ddos_protector(ddp_rule_id, base_url, api_key, key_header='
         result = requests.delete(base_url + '/rules/' + str(ddp_rule_id), headers={key_header: api_key})
         return result
     return None
+
+
+def get_rule_from_ddos_protector(ddp_rule_id, base_url, api_key, key_header='x-api-key') -> Optional[Response]:
+    """Remove a DDoS Protector rule from a DDoS Protector device using DDoS Protector's REST API
+    If the app is in testing mode, returns None and does not send anything.
+
+    :param ddp_rule_id: ID of the rule to remove. Should be a rule ID from DDoS Protector.
+    :param base_url: URL of the REST API endpoint to send the rule to
+    :param api_key: API key for the api
+    :param key_header: HTTP header name, where the API key should be included
+    :return: requests.Response object or None if app is in testing mode
+    :raise: ConnectionError - Given URL was not available
+    """
+    if not current_app.config['TESTING']:
+        result = requests.get(base_url + '/rules/' + str(ddp_rule_id), headers={key_header: api_key})
+        return result
+    return None
