d9ccb0e81e4cd95dd2529ca2c40dae9f14b8268d
Jakub Man <jakubman1@gmail.com>
2022-07-28 08:10:01 +0200

    add function to show range selection modal

--- flowapp/static/ts/forms.ts
+++ flowapp/static/ts/forms.ts
@@ -1,3 +1,5 @@
+import {Modal} from "bootstrap";
+
 /***
  * Add the 'd-none' CSS class to a specified element based on a condition
  * that the sourceValue is not equal to the showValue. Otherwise, remove
@@ -37,5 +39,59 @@ export function switchFocusOnKeypress(event: KeyboardEvent, key: string, focusId
         if (input) {
             input.focus();
         }
+    }
+}
+
+/***
+ * Show a bootstrap modal window identified by css ID #orgSelectModal.
+ * The modal is specified as a jinja macro "fill_org_form" in the
+ * flowapp/templates/forms/macros.j2 file.
+ *
+ * Calling this function opens the modal and registers a callback to
+ * its "Use range" button. The callback reads the value from the
+ * range selection input and applies it to appropriate input fields.
+ *
+ * @param {string} ipInputId   - id of the input field,
+ *                               where organization IP address should be filled to
+ * @param {string} maskInputId - id of the input field,
+ *                               where organization IP mask should be filled to
+ */
+export function fillOrganization(ipInputId: string, maskInputId: string): void {
+    const modalContainer = document.getElementById('orgSelectModal');
+    if (!modalContainer) {
+        return;
+    }
+    const modal = new Modal(modalContainer, {backdrop: true});
+    modal.show();
+    const btn = document.getElementById('fill-org-btn');
+    if (btn) {
+        btn.onclick = function () {
+            fillOrgValue(
+                document.getElementById(ipInputId) as HTMLInputElement,
+                document.getElementById(maskInputId) as HTMLInputElement
+            );
+            modal.hide();
+        }
+    }
+}
+
+/***
+ * Fill value from the selected organization range to appropriate fields.
+ * If a field is null, the value is skipped.
+ *
+ * @param {HTMLInputElement} ipField - A field to input the IP value to
+ * @param {HTMLInputElement} maskField - A field to input the mask value to
+ */
+function fillOrgValue(ipField: HTMLInputElement | null, maskField: HTMLInputElement | null) {
+    const org_select = document.getElementById('orgSelect') as HTMLInputElement;
+    if (!org_select) {
+        return;
+    }
+    const val = org_select.value.split('/');
+    if (ipField) {
+        ipField.value = val[0];
+    }
+    if (maskField) {
+        maskField.value = val[1];
     }
 }
